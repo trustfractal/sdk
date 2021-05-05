6ce105908bfee8ee8dd59f718d562e3cca52433e
Fernando Mendes <fernando@mendes.codes>
2021-05-05 16:14:54 +0100

    Export types from the top-level

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,24 @@ import DIDContract from "./DIDContract";
 import FractalError from "./FractalError";
 import Utils from "./utils";
 
+import {
+  Address,
+  Hash,
+  HashTree,
+  HashWithNonce,
+  IAttestationRequest,
+  IClaim,
+  IClaimProperties,
+  IClaimPseudoSchema,
+  IClaimSchema,
+  IClaimType,
+  ICredential,
+  Property,
+  Signature,
+} from "./types";
+
 export {
+  Address,
   AttestationRequest,
   Claim,
   ClaimType,
@@ -15,5 +32,17 @@ export {
   Crypto,
   DIDContract,
   FractalError,
+  Hash,
+  HashTree,
+  HashWithNonce,
+  IAttestationRequest,
+  IClaim,
+  IClaimProperties,
+  IClaimPseudoSchema,
+  IClaimSchema,
+  IClaimType,
+  ICredential,
+  Property,
+  Signature,
   Utils,
 };
