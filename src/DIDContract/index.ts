ab01244ff5ef2fbfc53c6d55065d1d37b98322ea
Diogo Couto <diogo2couto@gmail.com>
2021-05-24 19:23:27 +0100

    Fix did contract address to be dynamic

--- src/DIDContract/index.ts
+++ src/DIDContract/index.ts
@@ -6,11 +6,6 @@ import ContractData from "./Contract.js";
 export default class DIDContract {
   public static ABI = ContractData.abi;
 
-  public static CONTRACT_ADDRESSES: Record<string, string> = {
-    ropsten: "0x181573a13F4BF5F76F6d09D0E2a7716F6929993A",
-    mainnet: "0x1A5FA65E50d503a29Ec57cD102f2e7970a6963BB",
-  };
-
   public static toBuffer(str: string) {
     return ethersUtils.arrayify(str);
   }
@@ -19,15 +14,14 @@ export default class DIDContract {
   public contract: Contract;
 
   public constructor(
-    network: string,
+    address: string,
     {
       provider,
       providerUrl,
     }: { provider?: providers.Provider; providerUrl?: string }
   ) {
-    const address = DIDContract.CONTRACT_ADDRESSES[network];
 
-    if (!address) throw new Error(`Invalid network ${network}`);
+    if (!address) throw new Error(`Invalid address ${address}`);
 
     if (!provider && !providerUrl)
       throw new Error("Missing either provider or provider URL");
