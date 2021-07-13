04efe520718a3edb25c557bb2a925869e7572630
Fernando Mendes <fernando@mendes.codes>
2021-07-13 14:53:00 +0100

    Fix failing tests

--- src/SelfAttestedClaim/KYCTypes.ts
+++ src/SelfAttestedClaim/KYCTypes.ts
@@ -2,22 +2,19 @@ import { Byte } from "../types/Byte";
 
 import FractalError from "../FractalError";
 
-const TypesList = [
+const SupportedKYCs = [
   "plus+liveness+wallet",
   "plus+liveness+wallet+sow",
   "plus+selfie+wallet",
   "plus+selfie+wallet+sow",
-  "light+selfie+wallet",
-  "light+selfie+wallet+sow",
   "plus+liveness+accreditation+wallet",
   "plus+liveness+accreditation+wallet+sow",
   "plus+selfie+accreditation+wallet",
   "plus+selfie+accreditation+wallet+sow",
-  "light+selfie+accreditation+wallet",
 ];
 
 const ToByte = (type: string): Byte => {
-  const index = TypesList.indexOf(type);
+  const index = SupportedKYCs.indexOf(type);
 
   if (index < 0) throw FractalError.unsupportedKycType(type);
 
